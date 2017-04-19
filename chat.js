748ac7ae6f38eca53999d018bf12c0a5ad43f7c1
Henrique Dias <hacdias@gmail.com>
2017-04-19 23:10:18 +0100

    updates

--- chat.js
+++ chat.js
@@ -1,25 +1,63 @@
-const WebSocket = require('ws');
-const channel = "C50SNCVSR";
-const Slack = require('@slack/client');
+var WebSocket = require('ws')
+var channel = 'C50SNCVSR'
+var slack = require('@slack/client')
 
-var rtm = null;
+var rtm = null
+var botID = ''
 
-function handleSocket(conn) {
-  var randId = 'rand' + new Date().getTime();
-  conn.on('message', function incoming(message) {
-    rtm.sendMessage(message, channel);
-  });
+var threads = {}
+
+function handleSocket (conn) {
+  var thread = ''
+
+  conn.on('message', function incoming (message) {
+    if (thread === '') {
+      rtm.sendMessage(message, channel, (err, res) => {
+        if (err != null) console.log('There was an error!')
+        thread = res.ts
+        threads[thread] = conn
+      })
+
+      return
+    }
+
+    rtm.send({
+      type: 'message',
+      channel: channel,
+      text: message,
+      thread_ts: thread
+    }, (err, res) => {
+      if (err != null) console.log('There was an error!')
+    })
+  })
+
+  conn.on('close', function open () {
+    rtm.send({
+      type: 'message',
+      channel: channel,
+      text: '*My fan just went offline.*',
+      thread_ts: thread
+    }, (err, res) => {
+      if (err != null) console.log('There was an error!')
+    })
+    delete threads[thread]
+  })
 }
 
-function handleSlack(message) {
-  if (message.channel != channel) return;
-  console.log(message)
+function handleSlack (message) {
+  if (message.subtype === 'message_replied') return
+  if (message.user === botID) return
+
+  if (threads.hasOwnProperty(message.thread_ts)) {
+    threads[message.thread_ts].send(message.text)
+  }
 }
 
-module.exports = (r) => {
-  rtm = r;
+module.exports = (r, i) => {
+  rtm = r
+  botID = i
 
-  var socket = new WebSocket.Server({ port: 80 });
-  socket.on('connection', handleSocket);
-  rtm.on(Slack.RTM_EVENTS.MESSAGE, handleSlack);
-};
+  var socket = new WebSocket.Server({ port: 80 })
+  socket.on('connection', handleSocket)
+  rtm.on(slack.RTM_EVENTS.MESSAGE, handleSlack)
+}
--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS
 
-var RtmClient = require('@slack/client').RtmClient;
-var rtm = new RtmClient(process.env.SLACK_BOT_TOKEN);
+var RtmClient = require('@slack/client').RtmClient
+var rtm = new RtmClient(process.env.SLACK_BOT_TOKEN)
+
+var botID = ''
+
+var users = {}
+var firstMessage = false
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}.`);
-  require('./chat')(rtm)
-});
-
-rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  /*  console.log('Message:', message);
-
-
-  if (message.channel != livechatChannel) return;
-
-  if (message.text == "") return
-
-  rtm.sendTyping(message.channel)
-
-  if (message.subtype == "message_replied") {
-    rtm.send({
-      type: 'message',
-      channel: message.channel,
-      text: "I'm a fucking thread!",
-      thread_ts: message.message.ts,
-    })
+  for (let user of rtmStartData.users) {
+    if (user.deleted || user.is_bot) continue
+    users[user.id] = user.profile
   }
- */
 
- //this is no doubt the lamest possible message handler, but you get the idea
-  //rtm.sendMessage(message.text, "#testes");
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}.`)
+  botID = rtmStartData.self.ID
+  require('./chat')(rtm, botID)
+})
 
-/*   rtm.sendMessage(message.text, message.channel, (err, res) => {
-    console.log(err)
-    console.log(res) // da para obter a TS aqui para começar a criar threads... OMFG
-  }); */
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  if (!firstMessage) {
+    // INVESTIGAR ISTO
+    firstMessage = true
+    return
+  }
 
-});
+  // Direct messages! Fuck yeah!
+  if (message.channel.startsWith('D')) {
+    let answer = ''
 
-rtm.start();
+    switch (message.text.toLowerCase()) {
+      case 'ping':
+        answer = 'pong'
+        break
+      case 'tell me a joke':
+        answer = "I'm not fully grown up yet! Sorry :sad:"
+        break
+      default:
+        answer = `Sorry ${users[message.user].first_name}, I didn't quite understand what you just said :disappointed:`
+    }
+
+    rtm.sendMessage(answer, message.channel)
+  }
+})
+
+rtm.start()
